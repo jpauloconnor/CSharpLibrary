b4363ac659db773b6cb1248ece9f4035592d8627
Paul O'Connor <jamespauloconnor@gmail.com>
2017-03-27 16:49:17 -0400

    Fix merge issue

---
+++ 0.26_OOP_Demo_Paul/Player.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._26_OOP_Demo_Paul
+{
+    public class Player
+    {
+        /// <summary>
+        /// Discuss stylecop order: http://stackoverflow.com/questions/150479/order-of-items-in-classes-fields-properties-constructors-methods
+        /// </summary>
+        /*
+         * Constant Fields
+            Fields
+            Constructors
+            Enums
+            Interfaces
+            Properties
+            Indexers
+            Methods
+            Structs
+            Classes
+         */
+        //CONSTANTS
+        const int MAX_ATTACK_LEVEL = 100;
+        const int MAX_DEFENSE_LEVEL = 100;
+        const int MIN_ATTACK_LEVEL = 0;
+        const int MIN_DEFENSE_LEVEL = 0;
+        const int HIGHEST_XP_LEVEL = 75;
+        const int POWER_LEVEL_CLOSE_TO_DYING = 10;
+
+        //FIELDS
+        //Player Status
+        int _currentPower = 100;
+        int _attackLevel = 0;
+        int _defenseLevel = 0;
+        bool _isAlive = true;
+        bool _isLowOnPower = true;
+        bool _isInBonusLand = true;
+        bool _hasRandomPowerUpLaser = true;
+
+        // Player details.
+        string _firstName = null;
+        string _gamerName = null;
+        int _gamerLevel = 0;
+        CharacterType _characterType = CharacterType.Undefined;
+
+        // CONSTRUCTORS
+        public Player()
+        {
+
+        }
+        public Player(string firstName, string gamerName, CharacterType type)
+        {
+            this.PlayerName = firstName;
+            this.GamerName = gamerName;
+            this.Type = type;
+        }
+
+        //ENUMS
+        // Custom type.
+        public enum CharacterType
+        {
+            BlitzenBlopper,
+            TrollCat,
+            HorseMage,
+            Soldier,
+            Undefined
+        }
+
+        // PROPERTIES
+        public string PlayerName { get; set; }
+        public string GamerName
+        {
+            get { return _gamerName; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name cannot be empty.");
+                _gamerName = value.Trim();
+            }
+        }
+        public bool IsDefinitelyLowOnPower
+        {
+            get
+            {
+                return _isLowOnPower;
+            }
+        }
+        public bool IsInBonusLand
+        {
+            get
+            {
+                return _isInBonusLand;
+            }
+        }
+        public bool NeedsPowerDesperately
+        {
+            get
+            {
+                return _currentPower <= POWER_LEVEL_CLOSE_TO_DYING;
+            }
+        }
+        public CharacterType Type
+        {
+            get
+            {
+                return _characterType;
+            }
+            set
+            {
+                if (value == CharacterType.Undefined)
+                    throw new ArgumentException("Character Type cannot be undefined.");
+                else
+                    _characterType = value;
+            }
+        }
+        public int PlayerPower { get; set; }
+
+        //METHODS
+        //Attack, Defend, Heal, Respawn
+
+
+        public string DeclareType()
+        {
+            if (this.Type.ToString() == CharacterType.HorseMage.ToString())
+            {
+                string message = "You are a Horse Mage";
+                Console.WriteLine(message);
+                return message;
+            }
+            else
+            {
+                return "hey";
+            }
+
+        }
+
+        public void AnotherDay()
+        {
+            Console.WriteLine("Maybe another day.");
+        }
+
+        public bool Attack()
+        {
+            if (!this.IsDefinitelyLowOnPower)
+            {
+                Console.WriteLine("Ready to attack!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("You are definitely low on attack power. You can't attack.");
+                return false;
+            }
+        }
+        public bool Defend()
+        {
+            return true;
+            //TODO
+        }
+        public bool AbleToHeal()
+        {
+            //TODO: Code
+            return true;
+        }
+
+        public int GetPowerLevel()
+        {
+            Random rando = new Random();
+            int power = rando.Next(1, 100);
+            this.PlayerPower = power;
+            Console.WriteLine(this.PlayerPower);
+            return PlayerPower;
+        }
+
+        public void StatePowerLevel()
+        {
+
+            Console.WriteLine("I am a level {0}", this.PlayerPower);
+        }
+
+
+
+        //public override string ToString() /* override so we print first name + last name when calling ToString() */
+        //{
+        //    return String.Format("this.PlayerName, this.GamerName, this.Type);
+        //}
+
+    }
+}
