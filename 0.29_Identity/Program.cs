7612fb715ce5030b8f131cc19acaa641979c030f
Paul O'Connor <jamespauloconnor@gmail.com>
2018-01-16 07:25:44 -0500

    Delegate items added

---
+++ 0.29_Identity/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace _0._29_Identity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            var result = userManager.Create(new IdentityUser("paul@paul.com"), "Test123");
+            var resultTwo = userManager.FindByName("paul@paul.com");
+            var resultThree = userManager.FindByName()
+            Console.WriteLine("Worked");
+            Console.WriteLine(resultTwo);
+            Console.ReadLine();
+        }
+    }
+}
