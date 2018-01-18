a2d81c6854011178af148b4f3b8aa6895aab3696
Paul O'Connor <jamespauloconnor@gmail.com>
2018-01-17 22:16:17 -0500

    Adding interview questions for PM

---
+++ 0.30_Interview_Questions_PM/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._30_Interview_Questions_PM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
